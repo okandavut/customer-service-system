2ba05fad0aec3aa4640bd60d953a7bbcafe80f4d
okandavut <davut.okan@gmail.com>
2020-09-10 17:38:43 +0300

    Added date formatter for create and update dates.

---
+++ customer-service-client/utils/dateFormatter.js
@@ -0,0 +1,14 @@
+function getFormattedDate(dateString) {
+  var date = new Date(dateString);
+  var year = date.getFullYear();
+
+  var month = (1 + date.getMonth()).toString();
+  month = month.length > 1 ? month : "0" + month;
+
+  var day = date.getDate().toString();
+  day = day.length > 1 ? day : "0" + day;
+
+  return month + "/" + day + "/" + year;
+}
+
+export default getFormattedDate;
