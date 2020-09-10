b3343a1e4554d10276a7ebb2c1ff0173f8bcc10e
okandavut <davut.okan@gmail.com>
2020-09-10 17:38:57 +0300

    Added modal for show detail for request

--- customer-service-client/pages/service/index.js
+++ customer-service-client/pages/service/index.js
@@ -1,26 +1,38 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-
+import { Modal, Button } from "react-bootstrap";
+import getFormattedDate from "../../utils/dateFormatter";
 const Service = (props) => {
   const [requests, setRequests] = useState([]);
+  const [requestDetails, setRequestDetails] = useState({});
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
 
   useEffect(() => {
     getCustomerRequests();
   }, []);
 
-  const getCustomerRequests = async () => {
+  async function getCustomerRequests() {
     const res = await axios.get("http://localhost:3000/customerRequests");
     setRequests(res.data);
-  };
+  }
 
-  function getRequestDetail(phoneNumber) {
-    console.log(phoneNumber);
+  async function getRequestDetail(phoneNumber) {
+    const res = await axios.post(
+      "http://localhost:3000/getCustomerRequestsByPhoneNumber",
+      { customerPhone: phoneNumber }
+    );
+    setRequestDetails({ description: res.data.description });
+    setShow(true);
+
+    console.log(res.data);
   }
 
   return (
     <div>
       <center>
-        <h1>Customer Requests</h1>
+        <h1 className="title">Customer Requests</h1>
       </center>
       <table className="service-list">
         <thead>
@@ -32,24 +44,48 @@ const Service = (props) => {
             <th>Phone Number</th>
             <th>Created At</th>
             <th>Updated At</th>
+            <th>Change</th>
           </tr>
         </thead>
         <tbody>
           {requests.map(function (item, i) {
             return [
-              <tr key={i}  onClick={getRequestDetail.bind(this, item.customerPhone)}>
+              <tr key={i}>
                 <td>{item.description}</td>
                 <td>{item.createdCustomerName}</td>
                 <td>{item.note}</td>
                 <td>{item.status}</td>
                 <td>{item.customerPhone}</td>
-                <td>{item.createdDate}</td>
-                <td>{item.updatedDate}</td>
+                <td>{getFormattedDate(item.createdDate)}</td>
+                <td>{getFormattedDate(item.updatedDate)}</td>
+                <td>
+                  <Button
+                    variant="primary"
+                    className="button"
+                    onClick={getRequestDetail.bind(this, item.customerPhone)}
+                  >
+                    Change State
+                  </Button>
+                </td>
               </tr>,
             ];
           })}
         </tbody>
       </table>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{requestDetails.description}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
